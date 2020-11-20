c4f9e25f7a6aea7b585bdb34aa12e658278d5e46
Frost <54192369+ItsFR0ST@users.noreply.github.com>
2020-11-20 19:14:34 +0200

    Update rickroll.js

--- src/commands/rickroll.js
+++ src/commands/rickroll.js
@@ -2,22 +2,23 @@ const Discord = require("discord.js")
 
 module.exports.run = async (client, message, args) => {
 
-   let user = message.guild.members.cache.get(args[1])  
+    let user = message.mentions.members.first() || message.guild.members.cache.get(args[0])
+    
     if(!user) return message.reply('Choose someone to **RickRoll** him \n Remember!! to use ID only just to rickroll someone without pinging him')
     
 
 
     const embed = new Discord.MessageEmbed()
         .setAuthor(`${message.author.username}`, message.author.displayAvatarURL({ dynamic: true }))
-        .setDescription(`You have been **RickRolled** by ${message.author.id}`)
+        .setDescription(`You have been **RickRolled** by ${message.author}`)
         .setImage(`https://cdn.discordapp.com/attachments/358441505712635905/774256520153661440/SPOILER_tenor.gif`)
         .setColor("RANDOM")
         .setTimestamp()
-  
+  user.send(embed);
         message.delete();
 
 
-    user.send(embed);
+
 
 
     message.channel.send(`Successfully sent to ${user.user.tag}`)
